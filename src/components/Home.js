eab0bdaac0245ab8164966e9a795a34e0569adfb
Deepak <deepakyaryan898@gmail.com>
2023-06-22 21:53:17 +0530

    First Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,7 +29,7 @@ const Home = ({ScrollReveal}) => {
 
   return (
       <div name="home" className='min-h-screen w-full'>
-        <div className='md:pt-56 pt-20 max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full w-full px-4 md:flex-row'>
+        <div className='md:space-x-48 md:pt-56 pt-20 max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full w-full px-4 md:flex-row'>
           <div className='left flex flex-col justify-center h-full'>
             <h3 className='text-xl font-bold py-2'>Hello, It's Me</h3>
             <h1 className='text-4xl md:text-5xl font-bold'>Deepak Kumar</h1>
@@ -50,7 +50,7 @@ const Home = ({ScrollReveal}) => {
           </div>
 
           <div className='right md:w-1/2 animate-pulse' >
-            <img className='rounded-2xl mx-auto w-2/3 md-w-full' src={img} alt="my profile" />
+            <img className='rounded-2xl mx-auto w-2/3 md-w-full shadow-lg shadow-cyan-500' src={img} alt="my profile" />
           </div>
 
         </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,53 +7,59 @@ import iochat from '../assets/iochat.JPG'
 import pdfimerger from '../assets/pdfimerger.JPG'
 import { AiOutlineArrowUp } from 'react-icons/ai'
 
-const Portfolio = ({ScrollReveal}) => {
+const Portfolio = ({ ScrollReveal }) => {
     const portfolio = [
         {
             id: 1,
             src: ionotes,
+            name: "ioNotes",
             demo: "https://ionotes.netlify.app/",
             code: "https://github.com/Deepak-yaryan/iNotes"
         },
         {
             id: 2,
             src: newspanda,
+            name: "NewsPanda",
             demo: "https://pandanews.netlify.app/",
             code: "https://github.com/Deepak-yaryan/NewsPanda"
         },
         {
             id: 3,
             src: bigimart,
+            name: "Bigg iMart",
             demo: "https://biggimart.netlify.app/",
             code: "https://github.com/Deepak-yaryan/BigMart"
         },
         {
             id: 4,
             src: textuttils,
+            name: "TextUttils",
             demo: "https://textuttils.netlify.app/",
             code: "https://github.com/Deepak-yaryan/TextUtils"
         },
         {
             id: 5,
             src: iochat,
+            name: "ioChat",
             demo: "https://iochatt.netlify.app/",
             code: "https://github.com/Deepak-yaryan/ioChat"
         },
         {
             id: 6,
             src: pdfimerger,
+            name: "PDF iMerger",
             demo: "https://pdfimerger.onrender.com/",
             code: "https://github.com/Deepak-yaryan/PDFiMerger"
         },
 
     ]
-    React.useEffect(()=>{
+    React.useEffect(() => {
         const sr = ScrollReveal();
-        sr.reveal('.heading', {origin: 'top', mobile: false})
-        sr.reveal('.content', {origin: 'bottom', mobile: false})
+        sr.reveal('.heading', { origin: 'top', mobile: false })
+        sr.reveal('.content', { origin: 'bottom', mobile: false })
         return () => {
             sr.destroy();
-          }
+        }
     }, [ScrollReveal])
 
     return (
@@ -66,19 +72,23 @@ const Portfolio = ({ScrollReveal}) => {
 
                 <div className='content grid sm:grid-cols-2 md:grid-cols-4 gap-12 px-12 sm:px-0 cursor-pointer'>
                     {
-                        portfolio.map(({ id, src, demo, code }) => (
-
-                            <div key={id} className='shadow-lg shadow-cyan-600 rounded-lg flex relative overflow-hidden'>
-                                <img className='rounded-md hover:scale-105 duration-200' src={src} alt="" />
-                                <div className='absolute bottom-0 left-0 w-full h-full hover:bg-gradient-to-b from-black  flex flex-col justify-end items-center md:translate-y-36 hover:translate-y-0 duration-200'>
-                                <div className='fixed top-0 right-0'>
-                                <AiOutlineArrowUp size={1}/>
+                        portfolio.map(({ id, src, demo, code, name }) => (
+                            <div>
+                                <div className='flex flex-col'>
+                                    <div className='text-center m-auto hover:scale-110 duration-200 px-1 mb-1.5 shadow-md shadow-cyan-500 rounded-sm'>{name}</div>
                                 </div>
-                                <div className='text-white md:mb-4 -mb-1'>
-                                    <a href={demo}><button className='bg-gradient-to-b from-cyan-200 to-cyan-600 md:text-lg text-sm mr-5 px-1 rounded-md border border-white hover:scale-110 duration-200'>Demo</button></a>
-                                    <a href={code}><button className='bg-gradient-to-b from-cyan-200 to-cyan-600 md:text-lg text-sm ml-5 px-1 rounded-md border border-white hover:scale-110 duration-200'>Code</button></a>
+                                <div key={id} className='shadow-lg shadow-cyan-600 rounded-lg flex relative overflow-hidden'>
+                                    <img className='rounded-md hover:scale-105 duration-200' src={src} alt="" />
+                                    <div className='absolute bottom-0 left-0 w-full h-full hover:bg-gradient-to-b from-black  flex flex-col justify-end items-center md:translate-y-36 hover:translate-y-0 duration-200'>
+                                        <div className='fixed top-0 right-0'>
+                                            <AiOutlineArrowUp size={1} />
+                                        </div>
+                                        <div className='text-white md:mb-4 -mb-1'>
+                                            <a href={demo}><button className='bg-gradient-to-b from-cyan-200 to-cyan-600 md:text-lg text-sm mr-5 px-1 rounded-md border border-white hover:scale-110 duration-200'>Demo</button></a>
+                                            <a href={code}><button className='bg-gradient-to-b from-cyan-200 to-cyan-600 md:text-lg text-sm ml-5 px-1 rounded-md border border-white hover:scale-110 duration-200'>Code</button></a>
+                                        </div>
+                                    </div>
                                 </div>
-                                </div> 
                             </div>
                         ))
                     }
